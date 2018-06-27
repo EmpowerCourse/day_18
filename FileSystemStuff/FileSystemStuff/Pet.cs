6838d758a578ad7dd44ff86a12ed13992c98ba01
Derick Linkous <dl@steadyscience.com>
2018-06-27 10:04:00 -0400

    Initial Day 18 content.

---
+++ FileSystemStuff/FileSystemStuff/Pet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSystemStuff
+{
+    public class Pet
+    {
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public bool IsSpotted { get; set; }
+		public string Color { get; set; }
+    }
+}
---
+++ FileSystemStuff/FileSystemStuff/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FileSystemStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var pet = new Pet()
+			{
+				Name = "Loretta",
+				Age = 5,
+				IsSpotted = true,
+				Color = "Brown"
+			};
+
+			var petJson = JsonConvert.SerializeObject(pet);
+
+			var path = @"C:\MyFiles\LorettaJson.txt";
+			File.WriteAllText(path, petJson);
+
+
+			var read = File.ReadAllText(path);
+			var pet2 = JsonConvert.DeserializeObject<Pet>(read);
+
+			
+		}
+
+		static void Example1()
+		{
+			//var name = "Third run!";
+			var fileName = "Jeff.txt";
+			var path = "C:\\MyFiles\\" + fileName;
+			if (File.Exists(path))
+			{
+				var read = File.ReadAllText(path);
+				Console.WriteLine(read);
+			}
+			else
+			{
+				File.WriteAllText(path, String.Empty);
+			}
+			File.Copy(path, path.Replace("Jeff", "Adam"));
+			Console.ReadKey();
+			//File.WriteAllText(path, name);
+			//var read = File.ReadAllText(path);
+		}
+	}
+}
